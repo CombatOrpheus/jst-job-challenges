a7c3d3cbab11172b43af35f3e5804d8b998a573a
Gabriel Oliveira <SantosGabriel28@Hotmail.com>
2020-06-23 18:03:38 -0300

    Continuing User model for Database

--- back-end/src/main/java/com/justa/challenge/models/User.java
+++ back-end/src/main/java/com/justa/challenge/models/User.java
@@ -2,14 +2,39 @@ package com.justa.challenge.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-@Getter @Setter @AllArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+        })
 public class User {
 
     //TODO Integrate database
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 20)
     private String username;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(max = 40)
     private String password;
 }
