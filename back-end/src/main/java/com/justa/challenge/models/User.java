8e407079e2a3fdf17380d61b023fafbfec532e8e
Gabriel Oliveira <SantosGabriel28@Hotmail.com>
2020-06-23 18:37:28 -0300

    Continuing User model for Database

--- back-end/src/main/java/com/justa/challenge/models/User.java
+++ back-end/src/main/java/com/justa/challenge/models/User.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 @Entity
@@ -29,12 +28,15 @@ public class User {
     private String username;
 
     @NotBlank
-    private String name;
-
-    @NotBlank
     private String email;
 
     @NotBlank
     @Size(max = 40)
     private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
